6e83da8c3023644ef8e838d742c530bdf0b71f98
Jan Frederick Eick <jan-frederick.eick@uni-weimar.de>
2016-05-18 14:56:54 +0200

    stashing

--- app/src/main/java/de/uni_weimar/benike/misex3/MainActivity.java
+++ app/src/main/java/de/uni_weimar/benike/misex3/MainActivity.java
@@ -3,154 +3,227 @@ package de.uni_weimar.benike.misex3;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.DashPathEffect;
-import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.SeekBar;
 
-import com.androidplot.Plot;
-import com.androidplot.util.PixelUtils;
-import com.androidplot.xy.XYSeries;
-import com.androidplot.xy.*;
+import com.androidplot.util.PlotStatistics;
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
 
-import java.text.DecimalFormat;
-import java.util.Observable;
-import java.util.Observer;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends Activity
+        implements SensorEventListener,
+                    SeekBar.OnSeekBarChangeListener{
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-    }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    private static final String TAG = MainActivity.class.getName();
 
-    }
+    private static final int HISTORY_SIZE = 30;            // number of points to plot in history
+    private SensorManager mSensorManager = null;
+    private Sensor accelSensor = null;
 
-    // redraws a plot whenever an update is received:
-    private class MyPlotUpdater implements Observer {
-        Plot plot;
+    private XYPlot accelHistoryPlot = null;
+    private SimpleXYSeries accelXHistorySeries = null;
+    private SimpleXYSeries accelYHistorySeries = null;
+    private SimpleXYSeries accelZHistorySeries = null;
+    private SimpleXYSeries accelMHistorySeries = null;
 
-        public MyPlotUpdater(Plot plot) {
-            this.plot = plot;
-        }
+    public static int SAMPLE_MIN_VALUE = 1;
+    public static int SAMPLE_MAX_VALUE = 1000;
+    public static int SAMPLE_STEP = 1;
 
+    int interval = SAMPLE_MIN_VALUE;
+    Handler handler;
+    boolean flag = false;
+    private final Runnable processSensors = new Runnable() {
         @Override
-        public void update(Observable o, Object arg) {
-            plot.redraw();
+        public void run() {
+            // Do work with the sensor values.
+
+            flag = true;
+            // The Runnable is posted to run again here:
+            handler.postDelayed(this, interval);
         }
-    }
+    };
 
-    private XYPlot dynamicPlot;
-    private MyPlotUpdater plotUpdater;
-    private SensorManager mSensormanager;
-    private Sensor mSensor;
-    private Thread myThread;
-
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
-        // android boilerplate stuff
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // get handles to our View defined in layout.xml:
-        dynamicPlot = (XYPlot) findViewById(R.id.dynamicXYPlot);
+        // setup the Accelerometer History plot:
+        accelHistoryPlot = (XYPlot) findViewById(R.id.accelPlot);
 
-        plotUpdater = new MyPlotUpdater(dynamicPlot);
+        accelXHistorySeries = new SimpleXYSeries("X");
+        accelXHistorySeries.useImplicitXVals();
+        accelYHistorySeries = new SimpleXYSeries("Y");
+        accelYHistorySeries.useImplicitXVals();
+        accelZHistorySeries = new SimpleXYSeries("Z");
+        accelZHistorySeries.useImplicitXVals();
+        accelMHistorySeries = new SimpleXYSeries("M");
+        accelMHistorySeries.useImplicitXVals();
 
-        // only display whole numbers in domain labels
-        dynamicPlot.getGraphWidget().setDomainValueFormat(new DecimalFormat("0"));
 
-        // getInstance and position datasets:
-        mSensormanager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensormanager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensormanager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_UI);
-        //SampleDynamicSeries sine1Series = new SampleDynamicSeries(data, 0, "Sine 1");
-        //SampleDynamicSeries sine2Series = new SampleDynamicSeries(data, 1, "Sine 2");
+        //accelHistoryPlot.setRangeBoundaries(-30, 30, BoundaryMode.FIXED);
+        accelHistoryPlot.setDomainBoundaries(0, 30, BoundaryMode.FIXED);
+        accelHistoryPlot.addSeries(accelXHistorySeries, new LineAndPointFormatter(Color.rgb(100, 100, 200), null, null, null));
+        accelHistoryPlot.addSeries(accelYHistorySeries, new LineAndPointFormatter(Color.rgb(100, 200, 100), null, null, null));
+        accelHistoryPlot.addSeries(accelZHistorySeries, new LineAndPointFormatter(Color.rgb(200, 100, 100), null, null, null));
+        accelHistoryPlot.addSeries(accelMHistorySeries, new LineAndPointFormatter(Color.rgb(255, 255, 255), null, null, null));
+        accelHistoryPlot.setDomainStepValue(5);
+        accelHistoryPlot.setTicksPerRangeLabel(3);
+        accelHistoryPlot.setDomainLabel("Sample Index");
+        accelHistoryPlot.getDomainLabelWidget().pack();
+        accelHistoryPlot.setRangeLabel("m/s^2");
+        accelHistoryPlot.getRangeLabelWidget().pack();
 
-        LineAndPointFormatter formatter1 = new LineAndPointFormatter(
-                Color.rgb(0, 0, 0), null, null, null);
-        formatter1.getLinePaint().setStrokeJoin(Paint.Join.ROUND);
-        formatter1.getLinePaint().setStrokeWidth(10);
-        //dynamicPlot.addSeries(sine1Series,
-        //        formatter1);
+        // setup checkboxes:
+        final PlotStatistics levelStats = new PlotStatistics(1000, false);
+        final PlotStatistics histStats = new PlotStatistics(1000, false);
 
-        LineAndPointFormatter formatter2 =
-                new LineAndPointFormatter(Color.rgb(0, 0, 200), null, null, null);
-        formatter2.getLinePaint().setStrokeWidth(10);
-        formatter2.getLinePaint().setStrokeJoin(Paint.Join.ROUND);
+        accelHistoryPlot.addListener(histStats);
+        /*
+        hwAcceleratedCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) {
+                    aprLevelsPlot.setLayerType(View.LAYER_TYPE_NONE, null);
+                    accelHistoryPlot.setLayerType(View.LAYER_TYPE_NONE, null);
+                } else {
+                    aprLevelsPlot.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+                    accelHistoryPlot.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+                }
+            }
+        });
+        */
+        /*
+        showFpsCb = (CheckBox) findViewById(R.id.showFpsCb);
+        showFpsCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                levelStats.setAnnotatePlotEnabled(b);
+                histStats.setAnnotatePlotEnabled(b);
+            }
+        });
+        */
 
-        //formatter2.getFillPaint().setAlpha(220);
-        //dynamicPlot.addSeries(sine2Series, formatter2);
+        // get a ref to the BarRenderer so we can make some changes to it:
 
-        // hook up the plotUpdater to the data model:
-        //data.addObserver(plotUpdater);
+        // register for accelerometer events:
+        mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        for (Sensor sensor : mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER)) {
+            if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                accelSensor = sensor;
+            }
+        }
 
-        // thin out domain tick labels so they dont overlap each other:
-        dynamicPlot.setDomainStepMode(XYStepMode.INCREMENT_BY_VAL);
-        dynamicPlot.setDomainStepValue(5);
+        // if we can't access the accelerometer sensor then exit:
+        if (accelSensor == null) {
+            System.out.println("Failed to attach to accelSensor.");
+            cleanup();
+        }
 
-        dynamicPlot.setRangeStepMode(XYStepMode.INCREMENT_BY_VAL);
-        dynamicPlot.setRangeStepValue(10);
 
-        dynamicPlot.setRangeValueFormat(new DecimalFormat("###.#"));
+        handler = new Handler();
+        SeekBar seekBar = (SeekBar) findViewById(R.id.sampleRateSeekBar);
 
-        // uncomment this line to freeze the range boundaries:
-        dynamicPlot.setRangeBoundaries(-100, 100, BoundaryMode.FIXED);
+        seekBar.setMax( (SAMPLE_MAX_VALUE - SAMPLE_MIN_VALUE) / SAMPLE_STEP );
+        seekBar.setOnSeekBarChangeListener(this);
+        //mSensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, accelSensor, 1000000);
 
-        // create a dash effect for domain and range grid lines:
-        DashPathEffect dashFx = new DashPathEffect(
-                new float[] {PixelUtils.dpToPix(3), PixelUtils.dpToPix(3)}, 0);
-        dynamicPlot.getGraphWidget().getDomainGridLinePaint().setPathEffect(dashFx);
-        dynamicPlot.getGraphWidget().getRangeGridLinePaint().setPathEffect(dashFx);
+        Log.d(TAG, "Sensor delay UI: " + SensorManager.SENSOR_DELAY_UI);
+        Log.d(TAG, "Sensor delay fastest: " + SensorManager.SENSOR_DELAY_FASTEST);
+        Log.d(TAG, "Sensor delay normal: " + SensorManager.SENSOR_DELAY_NORMAL);
     }
+
     @Override
-    public void onResume() {
-        // kick off the data generating thread:
+    protected void onResume() {
         super.onResume();
+        mSensorManager.registerListener(this, accelSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        handler.post(processSensors);
     }
 
     @Override
-    public void onPause() {
+    protected void onPause() {
+        handler.removeCallbacks(processSensors);
+
         super.onPause();
     }
 
-    class SampleDynamicSeries implements XYSeries {
-        private int seriesIndex;
-        private String title;
-        private Sensor sensor;
-
-        public SampleDynamicSeries(Sensor sensor, int seriesIndex, String title) {
-            this.sensor = sensor;
-            this.seriesIndex = seriesIndex;
-            this.title = title;
-        }
-
-        @Override
-        public String getTitle() {
-            return title;
-        }
-
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public Number getX(int index) {
-            return 0;
-        }
-
-
-        @Override
-        public Number getY(int index) {
-            return 0;
-        }
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int value = SAMPLE_MIN_VALUE + progress * SAMPLE_STEP;
+        Log.d(TAG, "Sample value: " + value + "ms");
+        interval = value;
+        //mSensorManager.unregisterListener(this);
+        //mSensorManager.registerListener(this, accelSensor,  value);
     }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
+
+    private void cleanup() {
+        // unregister with the orientation sensor before exiting:
+        mSensorManager.unregisterListener(this);
+        finish();
+    }
+
+    // Called whenever a new accelSensor reading is taken.
+    @Override
+    public synchronized void onSensorChanged(SensorEvent sensorEvent) {
+
+        if (!flag) {
+            return;
+        }
+
+        flag = false;
+
+        // update instantaneous data:
+        Number[] series1Numbers = {sensorEvent.values[0], sensorEvent.values[1], sensorEvent.values[2]};
+
+        // get rid the oldest sample in history:
+        if (accelZHistorySeries.size() > HISTORY_SIZE) {
+            accelZHistorySeries.removeFirst();
+            accelYHistorySeries.removeFirst();
+            accelXHistorySeries.removeFirst();
+            accelMHistorySeries.removeFirst();
+        }
+
+        // add the latest history sample:
+        final float accelXdata = sensorEvent.values[0];
+        final float accelYdata = sensorEvent.values[1];
+        final float accelZdata = sensorEvent.values[2];
+        accelXHistorySeries.addLast(null, accelXdata);
+        accelYHistorySeries.addLast(null, accelYdata);
+        accelZHistorySeries.addLast(null, accelZdata);
+        accelMHistorySeries.addLast(null, Math.sqrt(accelXdata * accelXdata
+                + accelYdata * accelYdata + accelZdata * accelZdata)
+        );
+
+        // redraw the Plots:
+        accelHistoryPlot.redraw();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        // Not interested in this event
+    }
+
+
+
 }
